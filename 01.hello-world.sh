b502de0febbd8df24120d1997ea6bf89b4f26810
nagalingachary <sivanagalingachary@gmail.com>
2023-08-18 06:25:37 +0530

    Changes in commit

--- 01.hello-world.sh
+++ 01.hello-world.sh
@@ -2,7 +2,10 @@
 
 echo "Hello world,I am learning Devops and AWS"
 
-echo "Ramesh:Hi suresh,Good Morning"
-echo "Suresh:Very good morning Ramesh"
-echo "Ramesh:How are you Suresh"
-echo "Suresh:I am good,How are you Ramesh"
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:Hi $PERSON2,Good Morning"
+echo "$PERSON2:Very good morning $PERSON1"
+echo "$PERSON1:How are you $PERSON2"
+echo "$PERSON2:I am good,How are you $PERSON1"
