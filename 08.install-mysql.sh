b35e062e438ba88642d2de0579aa0834f43f8f20
nagalingachary <sivanagalingachary@gmail.com>
2023-08-18 20:50:09 +0530

    Changes in commit

--- 08.install-mysql.sh
+++ 08.install-mysql.sh
@@ -8,3 +8,13 @@ else
     echo "INFO:you are root user"
 fi
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installation of mysql is error"
+    exit 1
+else
+    echo "Installation of mysql is success"
+fi
+
+  
