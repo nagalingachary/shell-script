65a887f3cb794ed6a417c9e5f26c3ed0439ad122
nagalingachary <sivanagalingachary@gmail.com>
2023-08-18 06:22:01 +0530

    first commit

--- 01.hello-world.sh
+++ 01.hello-world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello world,I am learning Devops and AWS"
+
+echo "Ramesh:Hi suresh,Good Morning"
+echo "Suresh:Very good morning Ramesh"
+echo "Ramesh:How are you Suresh"
+echo "Suresh:I am good,How are you Ramesh"
