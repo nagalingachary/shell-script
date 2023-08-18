3d9739d1110c90679661df075af02e3be3a86a51
nagalingachary <sivanagalingachary@gmail.com>
2023-08-18 20:51:37 +0530

    Changes in commit

--- 08.install-mysql.sh
+++ 08.install-mysql.sh
@@ -17,4 +17,12 @@ else
     echo "Installation of mysql is success"
 fi
 
-  
+yum install postfix -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installation of postfix is error"
+    exit 1
+else
+    echo "Installation of postfix is success"
+fi
