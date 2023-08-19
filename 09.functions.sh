964d77224cd8c229b9e4e62acd3b90b582381f5c
nagalingachary <sivanagalingachary@gmail.com>
2023-08-19 18:37:55 +0530

    Changes in commit

--- 09.functions.sh
+++ 09.functions.sh
@@ -3,14 +3,17 @@
 DATE=$(date +%F-%H:%M:%S)
 SCRIPTNAME=$0
 LOGFILE=/tmp/$SCRIPTNAME-$DATE.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 VALIDATE() {
     if [ $1 -ne 0 ]
 then
-    echo "$2 is failure"
+    echo -e "$2 is $R failure $N"
     exit 1
 else
-    echo "$2 is Success"
+    echo -e "$2 is $G Success $N"
 fi
 }
 USERID=$(id -u)
