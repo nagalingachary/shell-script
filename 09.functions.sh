1cccb1fb3d5a87924cd073e54f307d1630b91345
nagalingachary <sivanagalingachary@gmail.com>
2023-08-19 07:36:02 +0530

    Changes in commit

--- 09.functions.sh
+++ 09.functions.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 USERID=$(id -u)
 
+VALIDATE(){
+    if [$1 -ne 0]
+then
+    echo "Installation is failure"
+    exit 1
+else
+    echo "Installation is Success"
+fi
+}
 if [$USERID -ne 0]
 then
     echo "ERROR::please run with root access"
 else
     echo "you are the root user"
 fi
+
 yum install mysql -y
 VALIDATE $?
 yum install postfix -y
